6fd84e6d32b31631f9a6e5a7506a1f994e4ef6f8
Amelie Oller <amelieoller@gmail.com>
2017-09-01 10:26:13 -0700

    Add list_events and menu methods to display events and details menu

--- lib/events_nearby/cli.rb
+++ lib/events_nearby/cli.rb
@@ -10,10 +10,36 @@ class EventsNearby::CLI
         puts "What city would you like to see events nearby?"
         input = gets.strip
         if input != "exit"
-            puts "This is a list of the events"
+            list_events(input)
         else
             goodbye
         end
+    end
+
+    def list_events(city)
+        puts "These are the upcoming events for #{city}:"
+        puts <<-DOC
+            1. Beer Tasting & Social Meetup | Sep 02 | San Francisco, CA | $30
+            2. Hofbrauhaus Munich Saturdays | Sep 02 | San Francisco, CA | Free
+            3. SF Bay Brazilian Day & Lavagem 2017 | Sep 03 | Berkeley, CA | $45
+            4. Labor Day Sail ~ Bloody Marys on San Francisco Bay | Sep 04 | Sausalito, CA | $75-$89
+            5. Brooklyn Meets The Bay Day Fest | Sep 04 | Oakland, CA | Free
+        DOC
+        menu
+    end
+
+    def menu
+        puts "Which event would you like to know more about? Enter a number or 'exit'"
+        input = gets.strip
+
+        if input.to_i.between?(1,5)  # Put events.all.size for how many events are in the array
+            puts "The selected event"
+        elsif input == "list"
+            list_events
+        else
+            puts "Not sure what you mean. Type 'list' or 'exit'."
+        end
+        goodbye
     end
 
     def goodbye
