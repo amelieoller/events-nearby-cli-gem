5d36329547815b9b7e3a8ee72b28f16bd929b155
Amelie Oller <amelieoller@gmail.com>
2017-09-01 16:59:38 -0700

    Remove unused truncate method

--- lib/events_nearby/scraper.rb
+++ lib/events_nearby/scraper.rb
@@ -35,19 +35,5 @@ class EventsNearby::Scraper
             event.save
         end
     end
-
-    def truncate(truncate_at, options = {})
-        return dup unless length > truncate_at
-    
-        options[:omission] ||= '...'
-        length_with_room_for_omission = truncate_at - options[:omission].length
-        stop =        if options[:separator]
-            rindex(options[:separator], length_with_room_for_omission) || length_with_room_for_omission
-        else
-            length_with_room_for_omission
-        end
-    
-        "#{self[0...stop]}#{options[:omission]}"
-    end
     
 end
