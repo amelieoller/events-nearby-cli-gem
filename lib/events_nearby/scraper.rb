39e0a1cc7e933403c786ffc44f66185b84260676
Amelie Oller <amelieoller@gmail.com>
2019-01-22 16:25:21 -0800

    Refactor scraper class to work with new website and city model

--- lib/events_nearby/scraper.rb
+++ lib/events_nearby/scraper.rb
@@ -1,47 +1,22 @@
-require_relative '../../config/environment'
-
 class EventsNearby::Scraper
+	def scrape_events(city)
+		puts "********* SCRAPING events *********"
+		url = "https://www.eventbrite.com/d/#{city.state}--#{city.name.split(/[\s,]+/).join("-")}/all-events/"
+		doc = Nokogiri::HTML(open("#{url}"))
 
-    def initialize(url = nil)
-        @url = url
-    end
+		doc.search(".search-event-card-wrapper").each do |event_data|
+			event = EventsNearby::Event.new
 
-    def scrape_event
-        @doc = Nokogiri::HTML(open(@url))
-        @doc.search(".js-xd-read-more-contents p").text.strip[0..300].gsub(/\s\w+\s*$/,'...')
-    end
+			event.name = event_data.search(".card-text--truncated__three").text.strip
+			event.url = event_data.search(".eds-media-card-content__action-link").attr('href').value
+			event.date = event_data.search(".eds-text-bs--fixed")[0].text
+			event.price = event_data.search(".eds-text-bs--fixed")[2].text
+		end
+	end
 
-    def scrape_events(location)
-        url = include_location_in_url(location)
-        @doc = Nokogiri::HTML(open("#{url}"))
-        @doc.search("div.js-event-list-container div.list-card-v2").each do |event_item|
-            event = EventsNearby::Events.new
-
-            event.name = event_item.search(".list-card__title").first.text.gsub("\n", "").strip
-            event.date = event_item.search(".list-card__date").first.text.gsub("\n", "").strip.split.join(" ")
-            event.price = event_item.search(".list-card__label").first.text.strip
-            event.url = event_item.attr("data-share-url").strip
-
-            event.save
-        end
-    end
-
-    def include_location_in_url(location)
-        data = location.split(/[\s,]+/)
-
-        if location.include?(",")
-            state = data.last.gsub(/[^0-9A-Za-z]/, '')
-            city_array = data.first data.size - 1
-        else
-            city_array = data
-        end
-        
-        city = city_array.collect do |item|
-            item.gsub(/[^0-9A-Za-z]/, '')
-        end.join("-")
-
-        url = "https://www.eventbrite.com/d/#{state}--#{city}/events/"
-        url
-    end
-    
+	def scrape_event(event)
+		puts "********* SCRAPING event *********"
+		doc = Nokogiri::HTML(open(event.url))
+		event.content = doc.search(".js-xd-read-more-contents p").text.strip[0..300].gsub(/\s\w+\s*$/,'...')
+	end
 end
