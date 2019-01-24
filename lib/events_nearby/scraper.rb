ec02ef1a26841564e7db9609ece3a4879e31cb0b
Amelie Oller <amelieoller@gmail.com>
2019-01-23 17:40:18 -0800

    Update scrape_events method to use new initalize and add nokogiri error handling

--- lib/events_nearby/scraper.rb
+++ lib/events_nearby/scraper.rb
@@ -1,22 +1,57 @@
 class EventsNearby::Scraper
-	def scrape_events(city)
-		puts "********* SCRAPING events *********"
-		url = "https://www.eventbrite.com/d/#{city.state}--#{city.name.split(/[\s,]+/).join("-")}/all-events/"
-		doc = Nokogiri::HTML(open("#{url}"))
+	def scrape_events(name, state)
+		url = "https://www.eventbrite.com/d/#{state}--#{name.split(/[\s,]+/).join("-")}/all-events/"
+
+		# Check if url is valid or returns error. If valid, scrape city, if not return false
+		doc = nokogiri_error_handling(url)
+		return false if doc == false
+
+		city = EventsNearby::City.new(name, state)
 
 		doc.search(".search-event-card-wrapper").each do |event_data|
-			event = EventsNearby::Event.new
+			name = event_data.search(".card-text--truncated__three").text.strip
+			url = event_data.search(".eds-media-card-content__action-link").attr('href').value
+			date = event_data.search(".eds-text-bs--fixed")[0].text
+			price = event_data.search(".eds-text-bs--fixed")[2].text
 
-			event.name = event_data.search(".card-text--truncated__three").text.strip
-			event.url = event_data.search(".eds-media-card-content__action-link").attr('href').value
-			event.date = event_data.search(".eds-text-bs--fixed")[0].text
-			event.price = event_data.search(".eds-text-bs--fixed")[2].text
+			event = EventsNearby::Event.new(name, date, price, url, city)
+			city.add_event(event)
 		end
+
+		city
 	end
 
 	def scrape_event(event)
-		puts "********* SCRAPING event *********"
-		doc = Nokogiri::HTML(open(event.url))
+		doc = nokogiri_error_handling(event.url)
 		event.content = doc.search(".js-xd-read-more-contents p").text.strip[0..300].gsub(/\s\w+\s*$/,'...')
+	end
+
+	def nokogiri_error_handling(url)
+		openuri_params = {
+			# set timeout durations for HTTP connection
+			# default values for open_timeout and read_timeout is 60 seconds
+			:open_timeout => 1,
+			:read_timeout => 1,
+		}
+
+		attempt_count = 0
+		max_attempts  = 3
+
+		begin
+			attempt_count += 1
+			content = open(url, openuri_params).read
+		rescue OpenURI::HTTPError => e
+			# it's 404, etc. - puts out error & do nothing
+			puts "We encountered a 404 error, please try again."
+			false
+		rescue SocketError, Net::ReadTimeout => e
+			# server can't be reached or doesn't send any respones
+			puts "error: #{e}"
+			sleep 3
+			retry if attempt_count < max_attempts
+		else
+			# connection successful, content is fetched, parse content with Nokogiri
+			Nokogiri::HTML(content)
+		end
 	end
 end
