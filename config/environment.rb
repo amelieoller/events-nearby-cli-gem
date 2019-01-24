8b15be79ba7a86ff058e66d97e5cc2db375f4a92
Amelie Oller <amelieoller@gmail.com>
2019-01-23 17:37:07 -0800

    Update environment files and remove obsolete file

--- config/environment.rb
+++
@@ -1,8 +0,0 @@
-# Load gems and modules
-require 'require_all'
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-
-# Load files
-require_all './lib'
---
+++ lib/environment.rb
@@ -0,0 +1,14 @@
+# Module EventsNearby
+module EventsNearby
+end
+
+# Load gems
+require 'require_all'
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require 'launchy'
+require 'colorize'
+
+# Load local files
+require_all './lib'
