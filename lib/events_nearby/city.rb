457132cd5d6d20fed9bd8fb1059ef5ec2e5bb33b
Amelie Oller <amelieoller@gmail.com>
2019-01-23 17:37:46 -0800

    Add events array, add_event method and comments to city model

--- lib/events_nearby/city.rb
+++ lib/events_nearby/city.rb
@@ -6,17 +6,25 @@ class EventsNearby::City
 	def initialize(name, state)
 		@name = name
 		@state = state
+		@events = []
 		@@all << self
+	end
+
+	# Adds new event to city array
+	def add_event(event)
+		self.events << event unless self.events.include?(event)
 	end
 
 	def self.all
 		@@all
 	end
 
+	# Find city if city and state combination exist already
 	def self.find_by(name, state)
 		self.all.detect{|city| city.name == name && city.state == state}
 	end
 
+	# Parse user city input and return name of city and state
 	def self.parse_city_input(city_input)
 		data = city_input.split(/[\s,]+/)
 
